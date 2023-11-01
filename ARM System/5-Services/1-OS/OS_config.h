ab906516b92194e804e55884e078c986d7d32198
kahrba23 <66400365+kahrba23@users.noreply.github.com>
2023-11-01 05:40:10 +0200

    Add files via upload
    Services layer

---
+++ ARM System/5-Services/1-OS/OS_config.h
@@ -0,0 +1,13 @@
+/******************************************************************/
+/* Author  : Ahmed Abdallah                                       */
+/* Date    : 23/9/2023                                            */
+/* SWC     : OS                                                   */
+/* Layer   : Services                                                  */
+/* Version : V01                                                  */
+/******************************************************************/
+#ifndef OS_CONFIG_H
+#define OS_CONFIG_H
+#define OS_NUMBER_OF_TASKS      3
+#define OS_FIRST_DELAY  0
+#define OS_SYS_TICK_NUMBER_OF_TICKS   1000
+#endif
